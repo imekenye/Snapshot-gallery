30fd9744790fd5367658b4a8854a08b134bae5fc
imekenye <ismekenye@gmail.com>
2019-05-10 14:52:29 -0700

    add methods to model files

--- imagegallery/models.py
+++ imagegallery/models.py
@@ -38,4 +38,19 @@ class Image(models.Model):
     def save_image(self):
         self.save()
 
+    @classmethod
+    def search_by_category(cls, search_term):  # search for an image using its category.
+        search_result = cls.objects.filter(image_category__cname__icontains=search_term)
+        return search_result
+
+    @classmethod
+    def filter_location(cls, location):  # filter images by the location.
+        filter_imagelocation = cls.objects.filter(image_location__lname__icontains=location)
+        return filter_imagelocation
+
+    @classmethod
+    def get_image_by_id(cls, input_id):
+        retrieved_image = cls.objects.get(id=input_id)
+        return retrieved_image
+
 
