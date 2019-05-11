b128ca4015fec47715e470836ce1d7fce9597e3d
imekenye <ismekenye@gmail.com>
2019-05-10 18:29:27 -0700

    update settings file

--- photogallery/settings.py
+++ photogallery/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'imagegallery.apps.ImagegalleryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
