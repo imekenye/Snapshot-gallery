1e8cbc5a8dccdeb532b7fd81dd113389f7b99d6d
imekenye <ismekenye@gmail.com>
2019-05-13 06:25:41 -0700

    add search in urlpatterns

--- imagegallery/urls.py
+++ imagegallery/urls.py
@@ -1,10 +1,9 @@
 from django.conf import settings
-from django.urls import path,include
+from django.urls import path, include
 
-from .import views
+from . import views
 
 urlpatterns = [
-    path('',views.index, name='index')
+    path('', views.index, name='index'),
+    path('search/', views.search_results, name='search_results'),
 ]
-
-
