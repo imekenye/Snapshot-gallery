6a4ebd55628e907cd2de5fb2b48fd3533450308e
imekenye <ismekenye@gmail.com>
2019-05-13 06:26:28 -0700

    update views file

--- imagegallery/views.py
+++ imagegallery/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
+from .models import Image,Location
 
 
 def index(request):
-    return render(request, 'pages/index.html')
+    images = Image.objects.all()
+    context = {
+        'images': images
+    }
+    return render(request, 'pages/index.html', context)
+
+
+def search_results(request):
+    locations = Location.objects.all()
+    if 'category' in request.GET and request.GET["category"]:
+        search_term = request.GET.get("category")
+        searched_images = Image.search_by_category(search_term)
+        message = f"Showing: {search_term} pictures"
+
+        return render(request, 'pages/search.html', {"message": message, "images": searched_images, "locations":locations})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'search.html', {"message": message})
+
+def specific_location(request, location):
+    locations = Location.objects.all()
+    location_results = Image.filter_location(location)
+    return render(request, 'search.html', {"locations": locations, "images": location_results})
